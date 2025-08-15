396df3231d254b652fbe11e4864f234caf927483
jose <joseluisariashc@gmail.com>
2025-08-15 10:03:25 +0200

    feat(color): crear modelo, migracion y CRUD de color

---
+++ src/app/Http/Controllers/ColorController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ColorRequest;
+use App\Models\Color;
+use Illuminate\Http\Request;
+
+class ColorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $colores = Color::paginate(config('web.paginacion_por_pagina'));
+        return response()->json([
+            'success' => true,
+            'data' => $colores
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ColorRequest $request)
+    {
+        $color = Color::create($request->validated());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Color creado correctamente',
+            'data' => $color
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Color $color)
+    {
+        return response()->json([
+            'success' => true,
+            'data' => $color
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ColorRequest $request, Color $color)
+    {
+        $color->update($request->validated());
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Color actualizado correctamente',
+            'data' => $color
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Color $color)
+    {
+        $color->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Color eliminado correctamente'
+        ]);
+    }
+}
