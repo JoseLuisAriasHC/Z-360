41f0cc9bd84e7e6983dd0039b39b4d8884cecccd
jose <joseluisariashc@gmail.com>
2025-08-23 12:00:40 +0200

    Feat(UserPhotoController): gestionar las fotos que suben los usuarios por producto

--- src/config/web.php
+++ src/config/web.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Paginación por página en las tablas de la administración de la web
+    |--------------------------------------------------------------------------
+    |
+    | Esta opción setea el numero de filas que se muestra en una página de
+    | una tabla en listados de la adminitración
+    |
+    */
+
     'paginacion_por_pagina' => env('PAGINACION_POR_PAGINA', 10),
 ];
