edc940febb77989ea6af4268e5e36d39011ed557
jose <joseluisariashc@gmail.com>
2025-08-25 11:16:11 +0200

    Feat(Etiqueta): gestionar las etiquetas (CRUD) y la asginacion y borrado de las etiquetas a los Products

---
+++ src/app/Models/Etiqueta.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Etiqueta extends Model
+{
+    use HasFactory;
+
+    protected $table = 'etiquetas';
+
+    protected $fillable = ['nombre'];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_etiquetas');
+    }
+}
