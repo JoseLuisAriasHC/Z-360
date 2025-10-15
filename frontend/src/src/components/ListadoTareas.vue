479744b318617fffc71780de784ff87246db28d3
jose <joseluisariashc@gmail.com>
2025-10-15 17:44:24 +0200

    config(Proyecto): prueba basica de vue

---
+++ frontend/src/src/components/ListadoTareas.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Listado de Tareas</h1>
+    <div>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea" />
+        <ul v-for="(tarea, index) in tareas" :key="index">
+            <li>{{ tarea }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+const tareas = ref<string[]>([]);
+const nuevaTarea = ref<string>('');
+
+function addTarea() {
+    if (nuevaTarea.value.trim() !== '') {
+        tareas.value.push(nuevaTarea.value.trim());
+        nuevaTarea.value = '';
+    }
+}
+</script>
+
+<style scoped></style>
