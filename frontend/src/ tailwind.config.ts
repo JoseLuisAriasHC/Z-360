37a962f93ae38f4f6353d74dd9db937ca990f1d0
jose <joseluisariashc@gmail.com>
2025-10-17 10:11:53 +0200

    config(tailwind): cambios para que tailwind css funcione de manera global en el front

--- frontend/src/ tailwind.config.ts
+++ frontend/src/ tailwind.config.ts
@@ -1,22 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: ['selector', '[class*="app-dark"]'],
     content: [
-        './index.html',
+        './index.html', 
         './src/**/*.{vue,js,ts,jsx,tsx}',
-        // Excluir el módulo admin para evitar conflictos
-        '!./src/modules/admin/**/*.{vue,js,ts,jsx,tsx}',
     ],
-    // Opcional: usar prefijo para evitar conflictos totalmente
     prefix: 'tw-',
     theme: {
-        extend: {},
         screens: {
             sm: '576px',
             md: '768px',
             lg: '992px',
             xl: '1200px',
-            '2xl': '1920px',
-        },
+            '2xl': '1920px'
+        }
     },
     plugins: [],
-};
+}
--- frontend/src/vite.config.ts
+++ frontend/src/vite.config.ts
@@ -1,19 +1,19 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import Components from 'unplugin-vue-components/vite';
 import { PrimeVueResolver } from 'unplugin-vue-components/resolvers';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         vueDevTools(),
+        tailwindcss(),
         Components({
             resolvers: [PrimeVueResolver()],
-            dts: true, // genera typescript definitions
+            dts: true,
         }),
     ],
     resolve: {
@@ -23,5 +23,9 @@ export default defineConfig({
             '@web': fileURLToPath(new URL('./src/modules/web', import.meta.url)),
             '@shared': fileURLToPath(new URL('./src/shared', import.meta.url)),
         },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
     },
 });
